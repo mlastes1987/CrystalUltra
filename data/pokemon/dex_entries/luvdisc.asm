c1829922b40a397bfdcd11097d33df5f9af97087
MARK\mlast <mlastes1987@gmail.com>
2025-03-02 10:56:48 -0500

    Luvdisc Gen 3 Backport (Thanks Squidgy!)

---
+++ data/pokemon/dex_entries/luvdisc.asm
@@ -0,0 +1,10 @@
+	db "RENDEZVOUS@" ; species name
+	dw 200, 192 ; height, weight
+
+	db   "During the spawning"
+	next "season, countless"
+	next "LUVDISC congregate"
+
+	page "at coral reefs"
+	next "turning the waters"
+	next "pink.@"
