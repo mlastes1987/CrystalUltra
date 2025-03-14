0f290866715869ae47771c7e7f2a71b144bedb98
TimKun55 <160082381+TimKun55@users.noreply.github.com>
2025-03-14 13:06:48 +1030

    Nayru's Pokédex v3.4.1 Credit: https://github.com/Nayru62/pokecrystal/tree/8.0_Nayrus_Pokedex
    Ported by https://github.com/TimKun55
    
    Features:
    
    Generation 3 Type Tiles
    Custom Pokédex Colours
    Toggle Shiny Palettes
    Unique Pokémon Icons
    Base stats + info
    Learn sets
    Detailed area info
    Evolution chart
    Sprite page showing animated sprites (reflects shiny toggle).
    
    Stats Page supports EVs, detects automatically
    rangi_dex_tiles, plus minor edits
    Pokédex Functionality "Hints" Dialogue from Crystal Legacy
    Craig's overlaying button banner idea
    PokéGear Time of Day Icons
    
    Extra information:
    
    Uncompress PokedexLZ and SlowpokeLZ, account for Expanded Townmap tiles
    Added Options File for increased compatibility
    Accounted for 'Increase Pokémon sprite animation size
    Compatible with `Expand Tileset to 255`
    
    Move Information: Lvl-Up Moves -> Field Moves (plus lvl learned and/or TM/HM/Move Tutor) -> Egg Moves -> TMs. I have fully functional code commented out to include HMs. But I decided not to since Field Moves serve the same purpose.
    
    Area Pages: Currently supports Johto/Kanto Walking/grass/surfing encounters plus their swarms. Fishing Rods information is also included. Right now, all it will show you is the relevant Fishing Group Name. To check your local Fishing Group, check your PokeGear.
    
    PokéGear: Now shows the detailed name of map you're currently in (based on Map Group & Map Number) and it also reads the map's attribute to see it's fishing group, if it has one.

--- ram/wram.asm
+++ ram/wram.asm
@@ -2959,7 +2959,10 @@ wStartSecond:: db
 
 wRTC:: ds 4
 
-	ds 4
+wPokedexEntryType::     db
+wPokedexEntryPageNum::  db
+wPokedexEvoStage2::		db
+wPokedexEvoStage3::     db
 
 wDST::
 ; bit 7: dst
@@ -2976,7 +2979,9 @@ wGameTimeFrames::  db
 
 wCurDay:: db
 
-	ds 1
+wPokedexShinyToggle::
+; bit 0: set if displaying shiny palettes
+ 	db
 
 wObjectFollow_Leader:: db
 wObjectFollow_Follower:: db
@@ -3078,7 +3083,7 @@ wPokegearFlags::
 	db
 wRadioTuningKnob:: db
 wLastDexMode:: db
-	ds 1
+wCurPokedexColor:: db ; current dex color
 wWhichRegisteredItem:: db
 wRegisteredItem:: db
 
