5937e627f523a80c57d0a97546d326473eb90955
RaJah239 <ravijagroo239@gmail.com>
2025-04-23 09:47:29 -0400

    Smashing rocks has a chance to contain items Credit: https://github.com/pret/pokecrystal/wiki/Smashing-rocks-has-a-chance-to-contain-items

--- engine/events/overworld.asm
+++ engine/events/overworld.asm
@@ -1363,11 +1363,17 @@ RockSmashScript:
 
 	callasm RockMonEncounter
 	readmem wTempWildMonSpecies
-	iffalse .done
+	iffalse .no_battle
 	randomwildmon
 	startbattle
 	reloadmapafterbattle
-.done
+.no_battle
+	callasm RockItemEncounter
+	iffalse .no_item
+	opentext
+	verbosegiveitem ITEM_FROM_MEM
+	closetext
+.no_item
 	end
 
 MovementData_RockSmash:
